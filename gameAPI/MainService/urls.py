38b730b1b70f9cb5e0ad13a668fc1a02a772364c
pouriyabp <pouriya1379@gmail.com>
2021-11-17 14:58:23 +0330

    add all API that in main service.

--- gameAPI/MainService/urls.py
+++ gameAPI/MainService/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('games-based-rank/', views.GamesBasedRankAPI.as_view(), name='games_based_rank'),
     path('games-based-name/', views.GamesBasedNameAPI.as_view(), name='games_based_name'),
     path('n-games-based-platform/', views.NGamesBasedPlatformAPI.as_view(), name='n_games_based_platform'),
+    path('n-games-based-year/', views.NGamesBasedYearAPI.as_view(), name='n_games_based_year'),
+    path('n-games-based-genre/', views.NGamesBasedGenreAPI.as_view(), name='n_games_based_genre'),
+    path('five-top-games/', views.FiveTopGamesAPI.as_view(), name='five_top_games'),
+    path('games-EU-sales-gt-NA-sales/', views.GamesEUgtNAAPI.as_view(),
+         name='games_EU_sales_gt_NA_sales/'),
 ]
