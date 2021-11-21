c50f1e5dd492f1b10ee67f1dcaabaa25c41b88be
mohammadsaeed0901 <mohammad.saeed.ka78@gmail.com>
2021-11-21 22:38:12 +0330

    fix bugs

--- gameAPI/User/models.py
+++ gameAPI/User/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class SignUp(models.Model):
     Username = models.CharField(max_length=128, null=False, blank=False)
-    TokenPublic = models.CharField(max_length=256, null=False, blank=False)
+    TokenPublic = models.CharField(
+        max_length=256, null=False, blank=False, unique=True)
 
     def __str__(self) -> str:
         return f"User: {self.Username}"
